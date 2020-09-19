66aeef484a106b678a8fe5fa3dce2d431321edd1
AndreOneti <andreluiz.onete@hotmail.com>
2020-09-19 15:35:31 -0300

    Add request offline.html with no network

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
 
+const cacheName = "app-shell-v1";
+const assetsToCache = [
+  "offline.html"
+];
+
+async function cacheStaticAssets() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(assetsToCache);
+}
+
+async function getChache(nameCache, file) {
+  const cache = await caches.open(nameCache);
+  return cache.match(file);
+}
+
 self.addEventListener("install", event => {
   console.log("[Service Worker] Installing service worker...", event);
+  event.waitUntil(cacheStaticAssets());
   self.skipWaiting();
 });
 
@@ -9,6 +25,15 @@ self.addEventListener("actvate", event => {
   return self.clients.claim();
 });
 
+async function networkFirst(request) {
+  try {
+    return await fetch(request);
+  } catch (error) {
+    return getChache(cacheName, "offline.html");
+  }
+}
+
 self.addEventListener("fetch", event => {
   console.log("[Service Worker] Fetch event: ", event.request.url);
+  event.respondWith(networkFirst(event.request));
 });
