9367ddb4f97268bdcf212d0ae6bf398cf505721a
AndreOneti <andreluiz.onete@hotmail.com>
2020-09-19 16:29:01 -0300

    Add assets and more

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-const cacheName = "app-shell-v2";
+const cacheName = "app-shell-v1";
 const assetsToCache = [
-  "offline.html",
-  "assets/images/pwa-logo.png"
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'assets/images/pwa-logo.png',
+  'assets/js/material.min.js',
+  'assets/css/style.css',
+  'assets/js/app.js',
+  'favicon.ico',
+  'index.html',
+  '/'
 ];
 
 function removeOldCache(key) {
